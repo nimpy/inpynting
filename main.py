327fba2c68f757f1e2d3b140c7bdba2261b8d041
nimpy <nina.zizakic@gmail.com>
2019-10-16 11:04:02 +0200

    minor modifications in main for running the inpainting (first commit after a while)

--- main.py
+++ main.py
@@ -204,13 +204,13 @@ def main():
     # TODO thresh_uncertainty should maybe be related to the patch size relative to the image size,
     #  also taking into account whether the descripotrs are used
     # inputs
-    patch_size = 8  # needs to be an even number
+    patch_size = 16  # needs to be an even number
     stride = patch_size // 2 #TODO fix problem when stride isn't exactly half of patch size!
-    thresh_uncertainty = 6755360 #10360 #5555360 #35360 #85360 #155360 # 6755360  #155360  # 100000 #155360 #255360 #6755360
+    thresh_uncertainty = 10360 #10360 #5555360 #35360 #85360 #155360 # 6755360  #155360  # 100000 #155360 #255360 #6755360
     max_nr_labels = 10
     max_nr_iterations = 10
-    use_descriptors = True
-    store_descriptors = True
+    use_descriptors = False
+    store_descriptors = False
     
     folder_path = '/home/niaki/Code/inpynting_images/Lenna'
     image_filename = 'Lenna.png'
@@ -239,8 +239,8 @@ def main():
     # mask_filename = 'pred.tif'
 
     folder_path = '/scratch/data/panel13'  # don't forget to also change the descriptor
-    image_filename = 'panel13_cropped3.png'
-    mask_filename = 'panel13_mask_cropped3.png'
+    image_filename = 'panel13_cropped1.png'
+    mask_filename = 'panel13_mask_cropped1.png'
 
     
     inpaint_image(folder_path, image_filename, mask_filename, patch_size, stride, thresh_uncertainty, max_nr_labels, max_nr_iterations, use_descriptors, store_descriptors)
